0f7a41177b0ed7619e51461aeba72c20133fe755
Tobias Ribizel <ribizel@kit.edu>
2020-03-28 17:13:01 +0100

    add reference tests using core interface

--- reference/test/factorization/par_ilut_kernels.cpp
+++ reference/test/factorization/par_ilut_kernels.cpp
@@ -54,6 +54,23 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 namespace {
 
 
+class DummyLinOp : public gko::EnableLinOp<DummyLinOp>,
+                   public gko::EnableCreateMethod<DummyLinOp> {
+public:
+    DummyLinOp(std::shared_ptr<const gko::Executor> exec,
+               gko::dim<2> size = gko::dim<2>{})
+        : EnableLinOp<DummyLinOp>(exec, size)
+    {}
+
+protected:
+    void apply_impl(const gko::LinOp *b, gko::LinOp *x) const override {}
+
+    void apply_impl(const gko::LinOp *alpha, const gko::LinOp *b,
+                    const gko::LinOp *beta, gko::LinOp *x) const override
+    {}
+};
+
+
 template <typename ValueIndexType>
 class ParIlut : public ::testing::Test {
 protected:
@@ -61,6 +78,8 @@ protected:
         typename std::tuple_element<0, decltype(ValueIndexType())>::type;
     using index_type =
         typename std::tuple_element<1, decltype(ValueIndexType())>::type;
+    using factorization_type =
+        gko::factorization::ParIlut<value_type, index_type>;
     using Dense = gko::matrix::Dense<value_type>;
     using Coo = gko::matrix::Coo<value_type, index_type>;
     using Csr = gko::matrix::Csr<value_type, index_type>;
@@ -99,11 +118,27 @@ protected:
                {{-1., 1.}, {-2., .2}, {-1., -.3}, {0., 0.}},
                {{1., -2.}, {-3., -.1}, {0., 0.}, {.1, 2.}}},
               ref)),
+          identity(gko::initialize<Csr>(
+              {{1., 0., 0.}, {0., 1., 0.}, {0., 0., 1.}}, ref)),
+          lower_tri(gko::initialize<Csr>(
+              {{1., 0., 0.}, {1., 1., 0.}, {1., 1., 1.}}, ref)),
+          upper_tri(gko::initialize<Csr>(
+              {{2., 1., 1.}, {0., -3., 1.}, {0., 0., 4.}}, ref)),
           mtx_system(gko::initialize<Csr>({{1., 6., 4., 7.},
                                            {2., -5., 0., 8.},
                                            {.5, -3., 6., 0.},
                                            {.2, -.5, -9., 0.}},
                                           ref)),
+          mtx_l_system(gko::initialize<Csr>({{1., 0., 0., 0.},
+                                             {2., 1., 0., 0.},
+                                             {.5, -3., 1., 0.},
+                                             {.2, -.5, -9., 1.}},
+                                            ref)),
+          mtx_u_system(gko::initialize<Csr>({{1., 6., 4., 7.},
+                                             {0., 1., 0., 8.},
+                                             {0., 0., 6., 0.},
+                                             {0., 0., 0., 1.}},
+                                            ref)),
           mtx_l(gko::initialize<Csr>({{1., 0., 0., 0.},
                                       {4., 1., 0., 0.},
                                       {-1., 0., 1., 0.},
@@ -131,14 +166,38 @@ protected:
                                                 ref)),
           mtx_l_it_expect(gko::initialize<Csr>({{1., 0., 0., 0.},
                                                 {2., 1., 0., 0.},
-                                                {.5, 0., 1., 0.},
-                                                {0., .1, -2.25, 1.}},
+                                                {.5, 6. / 17., 1., 0.},
+                                                {.2, .1, -2.45, 1.}},
                                                ref)),
           mtx_u_it_expect(gko::initialize<Csr>({{1., 0., 0., 0.},
-                                                {0., -5., 0., 0.},
+                                                {6., -17., 0., 0.},
                                                 {4., 0., 4., 0.},
-                                                {7., -6., 0., .6}},
-                                               ref))
+                                                {7., -6., 0., -.8}},
+                                               ref)),
+          mtx_l_small_expect(gko::initialize<Csr>({{1., 0., 0., 0.},
+                                                   {2., 1., 0., 0.},
+                                                   {.5, 6. / 17., 1., 0.},
+                                                   {0., 0., -153. / 116., 1.}},
+                                                  ref)),
+          mtx_u_small_expect(gko::initialize<Csr>({{1., 6., 4., 7.},
+                                                   {0., -17., -8., -6.},
+                                                   {0., 0., 116. / 17., 0.},
+                                                   {0., 0., 0., .0}},
+                                                  ref)),
+          mtx_l_large_expect(
+              gko::initialize<Csr>({{1., 0., 0., 0.},
+                                    {2., 1., 0., 0.},
+                                    {.5, 6. / 17., 1., 0.},
+                                    {0.2, 0.1, -153. / 116., 1.}},
+                                   ref)),
+          mtx_u_large_expect(
+              gko::initialize<Csr>({{1., 6., 4., 7.},
+                                    {0., -17., -8., -6.},
+                                    {0., 0., 116. / 17., -47. / 34.},
+                                    {0., 0., 0., -3043. / 1160.}},
+                                   ref)),
+          fact_fact(factorization_type::build().on(exec)),
+          tol{r<value_type>::value}
     {}
 
     template <typename Mtx>
@@ -205,7 +264,12 @@ protected:
     std::unique_ptr<Csr> mtx1_expect_thrm3;
     std::unique_ptr<ComplexCsr> mtx1_complex;
     std::unique_ptr<ComplexCsr> mtx1_expect_complex_thrm;
-    std::unique_ptr<Csr> mtx_system;
+    std::shared_ptr<Csr> identity;
+    std::shared_ptr<Csr> lower_tri;
+    std::shared_ptr<Csr> upper_tri;
+    std::shared_ptr<Csr> mtx_system;
+    std::unique_ptr<Csr> mtx_l_system;
+    std::unique_ptr<Csr> mtx_u_system;
     std::unique_ptr<Csr> mtx_l;
     std::unique_ptr<Csr> mtx_u;
     std::unique_ptr<Csr> mtx_lu;
@@ -213,6 +277,12 @@ protected:
     std::unique_ptr<Csr> mtx_u_add_expect;
     std::unique_ptr<Csr> mtx_l_it_expect;
     std::unique_ptr<Csr> mtx_u_it_expect;
+    std::unique_ptr<Csr> mtx_l_small_expect;
+    std::unique_ptr<Csr> mtx_u_small_expect;
+    std::unique_ptr<Csr> mtx_l_large_expect;
+    std::unique_ptr<Csr> mtx_u_large_expect;
+    std::unique_ptr<typename factorization_type::Factory> fact_fact;
+    gko::remove_complex<value_type> tol;
 };  // namespace
 
 TYPED_TEST_CASE(ParIlut, gko::test::ValueIndexTypes);
@@ -239,21 +309,21 @@ TYPED_TEST(ParIlut, KernelThresholdSelectMax)
 TYPED_TEST(ParIlut, KernelComplexThresholdSelect)
 {
     using value_type = typename TestFixture::value_type;
-    this->test_select(this->mtx1_complex, 5, sqrt(2), r<value_type>::value);
+    this->test_select(this->mtx1_complex, 5, sqrt(2), this->tol);
 }
 
 
 TYPED_TEST(ParIlut, KernelComplexThresholdSelectMin)
 {
     using value_type = typename TestFixture::value_type;
-    this->test_select(this->mtx1_complex, 0, 0.1, r<value_type>::value);
+    this->test_select(this->mtx1_complex, 0, 0.1, this->tol);
 }
 
 
 TYPED_TEST(ParIlut, KernelComplexThresholdSelectMax)
 {
     using value_type = typename TestFixture::value_type;
-    this->test_select(this->mtx1_complex, 9, sqrt(9.01), r<value_type>::value);
+    this->test_select(this->mtx1_complex, 9, sqrt(9.01), this->tol);
 }
 
 
@@ -331,11 +401,10 @@ TYPED_TEST(ParIlut, KernelAddCandidates)
 
     GKO_ASSERT_MTX_EQ_SPARSITY(res_mtx_l, this->mtx_l_add_expect);
     GKO_ASSERT_MTX_EQ_SPARSITY(res_mtx_u, this->mtx_u_add_expect);
-    GKO_ASSERT_MTX_NEAR(res_mtx_l, this->mtx_l_add_expect,
-                        r<value_type>::value);
-    GKO_ASSERT_MTX_NEAR(res_mtx_u, this->mtx_u_add_expect,
-                        r<value_type>::value);
+    GKO_ASSERT_MTX_NEAR(res_mtx_l, this->mtx_l_add_expect, this->tol);
+    GKO_ASSERT_MTX_NEAR(res_mtx_u, this->mtx_u_add_expect, this->tol);
 }
+
 
 TYPED_TEST(ParIlut, KernelComputeLU)
 {
@@ -343,19 +412,176 @@ TYPED_TEST(ParIlut, KernelComputeLU)
     using Coo = typename TestFixture::Coo;
     using value_type = typename TestFixture::value_type;
     auto mtx_l_coo = Coo::create(this->exec, this->mtx_system->get_size());
-    auto mtx_u_transp = this->mtx_u->transpose();
+    this->mtx_l_system->convert_to(mtx_l_coo.get());
+    auto mtx_u_transp = this->mtx_u_system->transpose();
     auto mtx_u_coo = Coo::create(this->exec, this->mtx_system->get_size());
-    this->mtx_l->convert_to(mtx_l_coo.get());
     auto mtx_u_csc = gko::as<Csr>(mtx_u_transp.get());
     mtx_u_csc->convert_to(mtx_u_coo.get());
 
     gko::kernels::reference::par_ilut_factorization::compute_l_u_factors(
-        this->ref, this->mtx_system.get(), this->mtx_l.get(), mtx_l_coo.get(),
-        mtx_u_csc, mtx_u_coo.get());
+        this->ref, this->mtx_system.get(), this->mtx_l_system.get(),
+        mtx_l_coo.get(), mtx_u_csc, mtx_u_coo.get());
 
-    GKO_ASSERT_MTX_NEAR(this->mtx_l, this->mtx_l_it_expect,
-                        r<value_type>::value);
-    GKO_ASSERT_MTX_NEAR(mtx_u_csc, this->mtx_u_it_expect, r<value_type>::value);
+    GKO_ASSERT_MTX_NEAR(this->mtx_l_system, this->mtx_l_it_expect, this->tol);
+    GKO_ASSERT_MTX_NEAR(mtx_u_csc, this->mtx_u_it_expect, this->tol);
+}
+
+
+TYPED_TEST(ParIlut, ThrowNotSupportedForWrongLinOp)
+{
+    auto lin_op = DummyLinOp::create(this->ref);
+
+    ASSERT_THROW(this->fact_fact->generate(gko::share(lin_op)),
+                 gko::NotSupported);
+}
+
+
+TYPED_TEST(ParIlut, ThrowDimensionMismatch)
+{
+    using Csr = typename TestFixture::Csr;
+    auto matrix = Csr::create(this->ref, gko::dim<2>{2, 3}, 4);
+
+    ASSERT_THROW(this->fact_fact->generate(gko::share(matrix)),
+                 gko::DimensionMismatch);
+}
+
+
+TYPED_TEST(ParIlut, SetStrategies)
+{
+    using Csr = typename TestFixture::Csr;
+    using factorization_type = typename TestFixture::factorization_type;
+    auto l_strategy = std::make_shared<typename Csr::merge_path>();
+    auto u_strategy = std::make_shared<typename Csr::classical>();
+
+    auto factory = factorization_type::build()
+                       .with_l_strategy(l_strategy)
+                       .with_u_strategy(u_strategy)
+                       .on(this->ref);
+    auto fact = factory->generate(this->mtx_system);
+
+    ASSERT_EQ(factory->get_parameters().l_strategy, l_strategy);
+    ASSERT_EQ(fact->get_l_factor()->get_strategy(), l_strategy);
+    ASSERT_EQ(factory->get_parameters().u_strategy, u_strategy);
+    ASSERT_EQ(fact->get_u_factor()->get_strategy(), u_strategy);
+}
+
+
+TYPED_TEST(ParIlut, IsConsistentWithComposition)
+{
+    auto fact = this->fact_fact->generate(this->mtx_system);
+
+    auto lin_op_l_factor =
+        static_cast<const gko::LinOp *>(gko::lend(fact->get_l_factor()));
+    auto lin_op_u_factor =
+        static_cast<const gko::LinOp *>(gko::lend(fact->get_u_factor()));
+    auto first_operator = gko::lend(fact->get_operators()[0]);
+    auto second_operator = gko::lend(fact->get_operators()[1]);
+
+    ASSERT_EQ(lin_op_l_factor, first_operator);
+    ASSERT_EQ(lin_op_u_factor, second_operator);
+}
+
+
+TYPED_TEST(ParIlut, GenerateIdentity)
+{
+    auto fact = this->fact_fact->generate(this->identity);
+
+    GKO_ASSERT_MTX_NEAR(fact->get_l_factor(), this->identity, this->tol);
+    GKO_ASSERT_MTX_NEAR(fact->get_u_factor(), this->identity, this->tol);
+}
+
+
+TYPED_TEST(ParIlut, GenerateDenseIdentity)
+{
+    using Dense = typename TestFixture::Dense;
+    auto dense_id = Dense::create(this->exec, this->identity->get_size());
+    this->identity->convert_to(dense_id.get());
+    auto fact = this->fact_fact->generate(gko::share(dense_id));
+
+    GKO_ASSERT_MTX_NEAR(fact->get_l_factor(), this->identity, this->tol);
+    GKO_ASSERT_MTX_NEAR(fact->get_u_factor(), this->identity, this->tol);
+}
+
+
+TYPED_TEST(ParIlut, GenerateLowerTri)
+{
+    auto fact = this->fact_fact->generate(this->lower_tri);
+
+    GKO_ASSERT_MTX_NEAR(fact->get_l_factor(), this->lower_tri, this->tol);
+    GKO_ASSERT_MTX_NEAR(fact->get_u_factor(), this->identity, this->tol);
+}
+
+
+TYPED_TEST(ParIlut, GenerateUpperTri)
+{
+    auto fact = this->fact_fact->generate(this->upper_tri);
+
+    GKO_ASSERT_MTX_NEAR(fact->get_l_factor(), this->identity, this->tol);
+    GKO_ASSERT_MTX_NEAR(fact->get_u_factor(), this->upper_tri, this->tol);
+}
+
+
+TYPED_TEST(ParIlut, GenerateWithExactSmallLimit)
+{
+    using factorization_type = typename TestFixture::factorization_type;
+    auto fact = factorization_type::build()
+                    .with_approximate_select(false)
+                    .with_fill_in_limit(0.75)
+                    .on(this->exec)
+                    ->generate(this->mtx_system);
+
+    GKO_ASSERT_MTX_NEAR(fact->get_l_factor(), this->mtx_l_small_expect,
+                        this->tol);
+    GKO_ASSERT_MTX_NEAR(fact->get_u_factor(), this->mtx_u_small_expect,
+                        this->tol);
+}
+
+
+TYPED_TEST(ParIlut, GenerateWithApproxSmallLimit)
+{
+    using factorization_type = typename TestFixture::factorization_type;
+    auto fact = factorization_type::build()
+                    .with_approximate_select(true)
+                    .with_fill_in_limit(0.75)
+                    .on(this->exec)
+                    ->generate(this->mtx_system);
+
+    GKO_ASSERT_MTX_NEAR(fact->get_l_factor(), this->mtx_l_small_expect,
+                        this->tol);
+    GKO_ASSERT_MTX_NEAR(fact->get_u_factor(), this->mtx_u_small_expect,
+                        this->tol);
+}
+
+
+TYPED_TEST(ParIlut, GenerateWithExactLargeLimit)
+{
+    using factorization_type = typename TestFixture::factorization_type;
+    auto fact = factorization_type::build()
+                    .with_approximate_select(false)
+                    .with_fill_in_limit(1.2)
+                    .on(this->exec)
+                    ->generate(this->mtx_system);
+
+    GKO_ASSERT_MTX_NEAR(fact->get_l_factor(), this->mtx_l_large_expect,
+                        this->tol);
+    GKO_ASSERT_MTX_NEAR(fact->get_u_factor(), this->mtx_u_large_expect,
+                        this->tol);
+}
+
+
+TYPED_TEST(ParIlut, GenerateWithApproxLargeLimit)
+{
+    using factorization_type = typename TestFixture::factorization_type;
+    auto fact = factorization_type::build()
+                    .with_approximate_select(true)
+                    .with_fill_in_limit(1.2)
+                    .on(this->exec)
+                    ->generate(this->mtx_system);
+
+    GKO_ASSERT_MTX_NEAR(fact->get_l_factor(), this->mtx_l_large_expect,
+                        this->tol);
+    GKO_ASSERT_MTX_NEAR(fact->get_u_factor(), this->mtx_u_large_expect,
+                        this->tol);
 }
 
 
